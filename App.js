d05ff371b7806182f08da6154f0734bb8e7651da
Devorah Simon <devorahrsimon@gmail.com>
2024-05-01 18:19:00 -0700

    initial homescreen

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const StarrySky = require('./assets/arrow_background.jpeg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imageSource={StarrySky} />
+      </View>
+      <Text style={{ color: '#fff'}}>
+        Hi Dio this is the title screen feel free to make it pretty!
+      </Text>
+      <View style={styles.footerContainer}>
+        <Button label="log in" />
+        <Button label="create account" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
